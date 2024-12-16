031d5bd28157b6a97f40c6085ca6533c3de6ab59
= <limmyji066@gmail.com>
2024-12-16 02:24:42 -0500

    document 81

--- python/81.py
+++ python/81.py
@@ -7,19 +7,28 @@ class Solution(object):
         """
         left = 0
         right = len(nums) - 1
+        # modified binary search, note that after every step we will still have a mini rotated sorted array
         while left <= right:
             pivot = left + (right - left) // 2
             if nums[pivot] == target:
                 return True
             if nums[left] <= nums[pivot]:
+                # if nums[left] == nums[pivot], we have found a smaller rotated sorted array
+                #   search this one instead
                 if nums[left] == nums[pivot]:
                     if self.search(nums[left:pivot], target):
                         return True
+                # this is the normal binary search case where we can set right to the pivot
                 if nums[pivot] >= target and nums[left] <= target:
                     right = pivot
                 else:
+                    # else, if target < nums[left] or nums[pivot] < target, make left smaller so that
+                    #   we get closer to base case
                     left = pivot + 1
+            # if nums[left] > nums[pivot] and nums[right] >= nums[pivot]
             elif nums[right] >= nums[pivot]:
+                # if nums[pivot] <= target <= nums[right] and nums[pivot] < nums[left]
+                #   make left = pivot, so that we have nums[left] <= target <= nums[right] (normal binary search case)
                 if nums[pivot] <= target and nums[right] >= target:
                     left = pivot
                 else:
